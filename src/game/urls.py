84c660657af5f4559254a9de4cfd44ba821d7f10
FredericoFerr <fred_ferr@hotmail.com>
2025-11-18 21:05:30 -0300

    projeto senacmon

--- src/game/urls.py
+++ src/game/urls.py
@@ -5,9 +5,10 @@ from .views import battle
 app_name = "game"
 
 urlpatterns = [
-    path("start/", views.start_view, name="start"),
-    path("state/", views.state_view, name="state"),
-    path("roll/", views.roll_view, name="roll"),
+    path("start/", views.start_view, name="start"),   
+    path("state/", views.state_view, name="state"),   
+    path("roll/", views.roll_view, name="roll"),      
     path("battle/", views.battle_view, name="battle"),
     path("battle/resolve/", battle.battle_resolve_view, name="battle_resolve"),
+    path("abandon/", views.abandon_view, name="abandon"),
 ]
